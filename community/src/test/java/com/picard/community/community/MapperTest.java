770d2e72821899f6d589af62d89adba2e840f1c2
Picard Xie <picardxie@foxmail.com>
2020-10-27 14:35:26 +0800

    mybatis配置

---
+++ community/src/test/java/com/picard/community/community/MapperTest.java
@@ -0,0 +1,48 @@
+package com.picard.community.community;
+
+import com.picard.community.community.dao.UserMapper;
+import com.picard.community.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com");
+        user.setCreateTime(new Date());
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+    @Test
+    public void updateStatus(){
+        int rows = userMapper.updateStatus(150,1);
+        System.out.println(rows);
+        rows = userMapper.updateHeader(150,"http://www.nowcoder.com/101");
+        System.out.println(rows);
+        rows = userMapper.updatePassword(150,"hello");
+        System.out.println(rows);
+    }
+}
