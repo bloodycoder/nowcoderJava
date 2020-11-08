5a790993697ce77a46e675b8faf256459503268a
Picard Xie <picardxie@foxmail.com>
2020-11-08 14:38:38 +0800

    spring整合kafka

---
+++ community/src/test/java/com/picard/community/community/KafkaTests.java
@@ -0,0 +1,40 @@
+package com.picard.community.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+/*
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+    @Autowired
+    KafkaProducer kafkaProducer;
+    @Test
+    public void testKafka() throws InterruptedException {
+        kafkaProducer.sendMsg("test","hello");
+        kafkaProducer.sendMsg("test","picard");
+        Thread.sleep(1000*10);
+    }
+}
+
+
+@Component
+class KafkaProducer{
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+    public void sendMsg(String topic,String content){
+        kafkaTemplate.send(topic,content);
+    }
+}
+@Component
+class KafkaConsumer{
+    @KafkaListener(topics={"test"})
+    public void handleMessage(ConsumerRecord record){
+        System.out.println(record.value());
+    }
+}*/
