a2698c6974dd6c8557b829932fe20f6eadf4b154
Picard Xie <picardxie@foxmail.com>
2020-11-09 20:46:27 +0800

    搜索引擎elastic Test

--- community/src/main/java/com/picard/community/community/CommunityApplication.java
+++ community/src/main/java/com/picard/community/community/CommunityApplication.java
@@ -3,9 +3,17 @@ package com.picard.community.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
 
+	@PostConstruct
+	public void init(){
+		//解决netty启动冲突的问题
+		// Netty4Utils.java
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
